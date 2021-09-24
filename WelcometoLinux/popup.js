45f7cb4cb12ba8ed41b91223e0dad0c045382251
John Archie Padrigo <62136363+john0931-st@users.noreply.github.com>
2021-09-24 19:37:17 +0800

    added new files

---
+++ WelcometoLinux/popup.js
@@ -0,0 +1,15 @@
+function togglePopupReg(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglePopupLog(){
+  document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopupReg1(){
+  document.getElementById("popup-3").classList.toggle("active");
+}
+
+function togglePopupLog2(){
+  document.getElementById("popup-4").classList.toggle("active");
+}
